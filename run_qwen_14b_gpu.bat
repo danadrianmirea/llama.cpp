4c1f0ac8bc8b1fed9faf15e919ad78bf1596c3d0
Dan Adrian Mirea <damirea84@gmail.com>
2026-03-30 22:48:41 +0300

    remove some bat files

--- run_qwen_14b_gpu.bat
+++
@@ -1,9 +0,0 @@
-build\bin\Release\llama-server.exe ^
-  -m "d:\models\qwen2.5-coder-14b-instruct-q4_0.gguf" ^
-  -ngl 999 ^
-  --host 127.0.0.1 ^
-  --port 8080 ^
-  -c 2048 ^
-  -b 512 ^
-  -t 8 ^
-  --no-warmup ^
